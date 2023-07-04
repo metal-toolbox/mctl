e488205de80a693b143857145260f252801598f8
Joel Rebello <jrebello@packet.com>
2023-07-04 10:38:53 +0200

    model/config: split OIDC config for clients

--- pkg/model/model.go
+++ pkg/model/model.go
@@ -4,24 +4,38 @@ const (
 	AttributeNSFirmwareSetLabels = "sh.hollow.firmware_set.labels"
 )
 
+type (
+	APIKind string
+)
+
+const (
+	ServerserviceAPI APIKind = "serverservice"
+	ConditionsAPI    APIKind = "conditions"
+)
+
 // Config struct holds the mctl configuration parameters
 type Config struct {
+
 	// File is configuration file path
-	File string
+	File          string
+	Serverservice *ConfigOIDC `mapstructure:"serverservice_api"`
+	Conditions    *ConfigOIDC `mapstructure:"conditions_api"`
+}
+
+type ConfigOIDC struct {
 	// ServerService is the Hollow server inventory store,
 	// https://github.com/metal-toolbox/hollow-serverservice
-	ServerserviceEndpoint string `mapstructure:"serverservice_endpoint"`
+	Endpoint string `mapstructure:"endpoint"`
 
-	// ConditionsEndpoint is the URL for the Condition Orchestrator API
-	ConditionsEndpoint string `mapstructure:"conditions_endpoint"`
+	// Disable skips OAuth setup
+	Disable bool `mapstructure:"disable"`
 
 	// ServerService OAuth2 parameters
-	OidcClientID       string `mapstructure:"oidc_client_id"`
-	OidcIssuerEndpoint string `mapstructure:"oidc_issuer_endpoint"`
-	OidcAudience       string `mapstructure:"oidc_audience"`
-
-	// Disable Oauth
-	DisableOAuth bool `mapstructure:"disable_oauth"`
+	ClientID         string   `mapstructure:"oidc_client_id"`
+	IssuerEndpoint   string   `mapstructure:"oidc_issuer_endpoint"`
+	AudienceEndpoint string   `mapstructure:"oidc_audience_endpoint"`
+	Scopes           []string `mapstructure:"oidc_scopes"`
+	PkceCallbackURL  string   `mapstructure:"oidc_pkce_callback_url"`
 }
 
 // Firmware includes a firmware version attributes and is part of FirmwareConfig
