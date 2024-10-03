7c05c62906c720d917c860497bfaa56e0374def7
Jake Schuurmans <jschuurmans@equinix.com>
2024-10-03 10:29:57 -0400

    Refactor MCTL Bios interface (prereq for bios set)

--- cmd/bios/reset.go
+++ cmd/bios/reset.go
@@ -1,11 +1,9 @@
 package bios
 
 import (
-	"context"
 	"log"
 
 	mctl "github.com/metal-toolbox/mctl/cmd"
-	"github.com/metal-toolbox/mctl/internal/app"
 	rctypes "github.com/metal-toolbox/rivets/condition"
 	"github.com/spf13/cobra"
 )
@@ -14,39 +12,11 @@ var resetCmd = &cobra.Command{
 	Use:   "reset",
 	Short: "Reset BIOS settings to default values",
 	Run: func(cmd *cobra.Command, _ []string) {
-		biosAction(cmd.Context(), biosFlags)
+		err := CreateBiosControlCondition(cmd.Context(), rctypes.ResetConfig)
+		if err != nil {
+			log.Fatal(err)
+		}
 	},
-}
-
-func biosAction(ctx context.Context, flags *biosActionFlags) {
-	theApp := mctl.MustCreateApp(ctx)
-
-	client, err := app.NewConditionsClient(ctx, theApp.Config.Conditions, theApp.Reauth)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	serverID, err := flags.ParseServerID()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	condition, err := flags.ToCondition()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	response, err := client.ServerConditionCreate(ctx, serverID, rctypes.BiosControl, *condition)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	conditionResp, err := mctl.ConditionFromResponse(response)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	log.Printf("status=%d msg=%s conditionID=%s", response.StatusCode, response.Message, conditionResp.ID)
 }
 
 func init() {
