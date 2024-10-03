fa31e37a7760e2dbbf2629e301ec0e40176d850f
Jake Schuurmans <jschuurmans@equinix.com>
2024-10-03 10:29:57 -0400

    Refactor MCTL Bios interface (prereq for bios set)

--- cmd/bios/bios.go
+++ cmd/bios/bios.go
@@ -1,11 +1,12 @@
 package bios
 
 import (
-	"encoding/json"
+	"context"
+	"log"
 
 	"github.com/google/uuid"
-	"github.com/metal-toolbox/conditionorc/pkg/api/v1/conditions/types"
 	mctl "github.com/metal-toolbox/mctl/cmd"
+	"github.com/metal-toolbox/mctl/internal/app"
 	rctypes "github.com/metal-toolbox/rivets/condition"
 	"github.com/spf13/cobra"
 )
@@ -18,24 +19,38 @@ type biosActionFlags struct {
 	serverID string
 }
 
-func (f *biosActionFlags) ToCondition() (*types.ConditionCreate, error) {
-	id, err := f.ParseServerID()
+func CreateBiosControlCondition(ctx context.Context, action rctypes.BiosControlAction) error {
+	theApp := mctl.MustCreateApp(ctx)
+
+	client, err := app.NewConditionsClient(ctx, theApp.Config.Conditions, theApp.Reauth)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	biosParams := rctypes.NewBiosControlTaskParameters(id, rctypes.ResetSettings)
-
-	params, err := json.Marshal(biosParams)
+	serverID, err := biosFlags.ParseServerID()
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	return &types.ConditionCreate{Parameters: params}, nil
+	params := rctypes.NewBiosControlTaskParameters(serverID, action)
+
+	response, err := client.ServerBiosControl(ctx, params)
+	if err != nil {
+		return err
+	}
+
+	conditionResp, err := mctl.ConditionFromResponse(response)
+	if err != nil {
+		return err
+	}
+
+	log.Printf("status=%d msg=%s conditionID=%s", response.StatusCode, response.Message, conditionResp.ID)
+
+	return err
 }
 
 func (f *biosActionFlags) ParseServerID() (uuid.UUID, error) {
-	return uuid.Parse(biosFlags.serverID)
+	return uuid.Parse(f.serverID)
 }
 
 var biosCmd = &cobra.Command{
